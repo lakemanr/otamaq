2235f126276471b2089223cf172e2feab0de0a94
lakemanr <lakemanr@list.ru>
2023-03-12 23:50:30 +0600

    removed quotes from sql code

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE restaurants (
-  "id" serial PRIMARY KEY,
-  "name" varchar UNIQUE NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (now())
+  id serial PRIMARY KEY,
+  name varchar UNIQUE NOT NULL,
+  created_at timestamp NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE clients (
-  "id" serial PRIMARY KEY,
-  "full_name" varchar NOT NULL,
-  "login" varchar UNIQUE NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (now())
+  id serial PRIMARY KEY,
+  full_name varchar NOT NULL,
+  login varchar UNIQUE NOT NULL,
+  created_at timestamp NOT NULL DEFAULT (now())
 );
--- db/migration/000002_add_dishes.up.sql
+++ db/migration/000002_add_dishes.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dishes (
-  "id" serial PRIMARY KEY,
-  "name" varchar NOT NULL,
-  "rest_id" int NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (now())
+  id serial PRIMARY KEY,
+  name varchar NOT NULL,
+  rest_id int NOT NULL,
+  created_at timestamp NOT NULL DEFAULT (now())
 );
 
 ALTER TABLE dishes ADD FOREIGN KEY (rest_id) REFERENCES restaurants (id);
